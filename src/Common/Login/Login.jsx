a833991e4699833e8b7603352875b6d100fbd30e
madankumar3041 <69312892+madankumar3041@users.noreply.github.com>
2021-05-13 10:58:28 +0530

    Update Login.jsx

--- src/Common/Login/Login.jsx
+++ src/Common/Login/Login.jsx
@@ -69,6 +69,7 @@ export function Login(props) {
             class="an"
             type="text"
             name="email_or_username"
+            placeholder="email"
             required
             onChange={handleChange}
           />
@@ -76,6 +77,7 @@ export function Login(props) {
           <input
             type="password"
             name="password"
+             placeholder="password"
             class="an"
             required
             onChange={handleChange}
